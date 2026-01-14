b37373ea15c3c52db1c07ed3b700af4da2ff7423
jmoo <moodierthanyou@gmail.com>
2026-01-13 21:47:47 -0800

    confirmed working on linux

---
+++ run_multiple.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Number of processes to launch
+N=2
+
+echo "Launching $N instances of ./build/debug/bin/eROIL_Tests.exe..."
+
+for ((i=0; i<N; i++)); do
+    echo "Starting instance $i"
+    gnome-terminal -- bash -c "./build/linux-debug/bin/eROIL_Tests $i; exec bash"
+done
+
+echo "Done."
+exit 0
